a85a673bfa96c43e2a8e92a3532a99815308c4b1
spjspj <spjspj4@gmail.com>
2021-11-28 00:09:51 +1100

    Update btc_wallet.bat

--- btc_wallet.bat
+++ btc_wallet.bat
@@ -2,11 +2,11 @@
 rem Running it
 echo Creating a Bitcoin Wallet with a known mnemonic
 rem https://raw.githubusercontent.com/libbitcoin/libbitcoin-explorer/master/img/wallet-commands.png
-bx mnemonic-to-seed one two three four five six seven eight nine ten eleven twelve > my.seed
+d:\D_Downloads\bitcoin\bx.exe mnemonic-to-seed one two three four five six seven eight nine ten eleven twelve > my.seed
 
-type my.seed | bx ec-new > myecprivate.key
-type myecprivate.key | bx ec-to-public > myecpublic.key
-type myecpublic.key | bx ec-to-address > mybitcoinaddress
+type my.seed | d:\D_Downloads\bitcoin\bx.exe ec-new > myecprivate.key
+type myecprivate.key | d:\D_Downloads\bitcoin\bx.exe ec-to-public > myecpublic.key
+type myecpublic.key | d:\D_Downloads\bitcoin\bx.exe ec-to-address > mybitcoinaddress
 
 
 echo =============================
@@ -23,11 +23,11 @@ type mybitcoinaddress
 echo =============================
 echo =============================
 echo Mine!
-type mybitcoinaddress | bx fetch-balance
-type mybitcoinaddress | bx fetch-history
+type mybitcoinaddress | d:\D_Downloads\bitcoin\bx.exe fetch-balance
+type mybitcoinaddress | d:\D_Downloads\bitcoin\bx.exe fetch-history
 
 echo =============================
 echo =============================
 echo Testing one
-bx fetch-balance 1JziqzXeBPyHPeAHrG4DCDW4ASXeGGF6p6
-bx fetch-history 1JziqzXeBPyHPeAHrG4DCDW4ASXeGGF6p6
+d:\D_Downloads\bitcoin\bx.exe fetch-balance 1JziqzXeBPyHPeAHrG4DCDW4ASXeGGF6p6
+d:\D_Downloads\bitcoin\bx.exe fetch-history 1JziqzXeBPyHPeAHrG4DCDW4ASXeGGF6p6
