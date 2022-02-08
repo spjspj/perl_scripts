e2ad8383f81b4ab5ec8b3331fe8018e1286f1b5a
spjspj <spjspj4@gmail.com>
2022-02-08 23:16:48 +1100

    Add files via upload

---
+++ bell_control/find_bad_files.bat
@@ -0,0 +1,1 @@
+dir /a /b /s | cut.pl stdin "(.ade|.adp|.apk|.appx|.appxbundle|.bat|.cab|.chm|.cmd|.com|.cpl|.dll|.dmg|.exe|.hta|.ins|.isp|.iso|.jar|.js|.jse|.lib|.lnk|.mde|.msc|.msi|.msix|.msixbundle|.msp|.mst|.nsh|.pif|.ps1|.scr|.sct|.shb|.sys|.vb|.vbe|.vbs|.vxd|.wsc|.wsf|.wsh)$"  0 grep
