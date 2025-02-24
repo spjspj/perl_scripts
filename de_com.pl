58f5d171ced7de2ede28b1c35e80545654679724
spjspj <spjspj4@gmail.com>
2025-02-24 21:39:33 +1100

    Update de_com.pl

--- de_com.pl
+++ de_com.pl
@@ -1,42 +1,40 @@
 #!/usr/bin/perl
-
-use 5.010;
+https://github.com/spjspj/perl_scripts/blob/master/de_com.pl
 use Compress::Raw::Zlib;
 use strict;
 use warnings;
 
-my $input = "This is a test string to be compressed and decompressed.";
-my $input2 = "SECOND This is a test string to be compressed and decompressed.";
+sub compress
+{
+    my $input = $_ [0];
+    my $deflate;
+    $deflate = Compress::Raw::Zlib::Deflate->new
+        (
+         -WindowBits   => 15, 
+         -AppendOutput => 1 
+        );
+    my $compressed_bytes = '';
+    my $status = $deflate->deflate($input, $compressed_bytes);
+    $status = $deflate->flush($compressed_bytes);
 
-# Compress the string
-my ($deflate, $inflate, $compressed, $decompressed);
+    my $hcb = unpack("H*", $compressed_bytes);
+    my $compressed = pack("H*", $hcb);
+    return $compressed;
+}
 
-$deflate = Compress::Raw::Zlib::Deflate->new
-(
-    -WindowBits   => 15, 
-    -AppendOutput => 1 
-);
+sub decompress
+{
+    my $compressed = $_ [0];
+    my $decompressed;
+    my $inflate = Compress::Raw::Zlib::Inflate->new ();
+    my $status = $inflate->inflate($compressed, $decompressed);
+    return $decompressed;
+}
 
-my $compressed_bytes = '';
-my $status = $deflate->deflate($input, $compressed_bytes);
-$status = $deflate->flush($compressed_bytes);
-
-my $hcb = unpack("H*", $compressed_bytes);
-print $hcb, " << hcb\n";
-my $compressed = pack("H*", $hcb);
-
-$inflate = Compress::Raw::Zlib::Inflate->new ();
-
-$status = $inflate->inflate($compressed, $decompressed);
-print ("Status: $status\n");
-print "aa) Decompressed string: $decompressed\n";
-
-$inflate = Compress::Raw::Zlib::Inflate->new(
-    -WindowBits   => 15, 
-    -AppendOutput => 1
-);
-
-$decompressed .= "\nSecond go:\n";
-#$compressed_bytes =~ s/$compressed_bytes/\r\n/mg;
-$status = $inflate->inflate($compressed_bytes, $decompressed);
-print "bb) Decompressed string ($status): $decompressed\n";
+# Main
+{
+    my $input = "This is a test string to be compressed and decompressed.";
+    my $compressed = compress ($input);
+    my $decompressed = decompress ($compressed);
+    print $decompressed;
+}
