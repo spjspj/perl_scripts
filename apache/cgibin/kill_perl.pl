60b465adb9f86a20698987ed7fc45cba588f5dc9
spjspj <spjspj4@gmail.com>
2025-03-14 14:00:09 +1100

    Update kill_perl.pl

--- apache/cgibin/kill_perl.pl
+++ apache/cgibin/kill_perl.pl
@@ -11,28 +11,64 @@ use strict;
 use warnings;
 
 print "Content-type: text/html; charset=iso-8859-1\n\n";
-print "Restart games on this server!<br>\n";
+print "Kill perl on this server!<br>\n";
 
+my $output;
+my %perl_ports;
+$perl_ports {"create_magic_card.pl"} = 33334;
+$perl_ports {"csv_analyse.pl"} = 3867;
+$perl_ports {"cthulhu.pl"} = 6728;
+$perl_ports {"decode_words.pl"} = 23128;
+$perl_ports {"describe_magic_cards.pl"} = 50000;
+$perl_ports {"draft_magic_auto_normal.pl"} = 60000;
+$perl_ports {"draft_magic_cube.pl"} = 40000;
+$perl_ports {"filter_magic.pl"} = 56789;
+$perl_ports {"get_magic_deck.pl"} = 60001;
+$perl_ports {"hearts.pl"} = 2718;
+$perl_ports {"mastermind.pl"} = 6663;
+$perl_ports {"math_game_web.pl"} = 1234;
+$perl_ports {"mk_pss_html.pl"} = 7732;
+$perl_ports {"nothanks.pl"} = 3967;
+$perl_ports {"purchased_cards.pl"} = 6723;
+$perl_ports {"quest.pl"} = 3672;
+$perl_ports {"roshambo.pl"} = 14159;
+$perl_ports {"secure_copy_paste_w_images.pl"} = 6725;
+$perl_ports {"solve_wordle.pl"} = 4590;
+$perl_ports {"ALL"} = "all";
+
+my $script;
+my $val;
+foreach $script (sort (keys (%perl_ports)))
+{
+    $val = $perl_ports {$script};
+    print ("<a href='/cgibin/kill_single_perl.pl?$val'>$script (Port is $val)</a><br>");
+}
+
+my $netstat = `netstat -a -n -b -o`;
+$netstat =~ s/\n//img;
+$netstat =~ s/TCP/\nTCP/img;
+
+while ($netstat =~ s/^(.*?)\n//m)
+{
+    my $line = $1;
+    if ($line =~ m/perl/)
+    {
+        $output .= $line . "\n";
+    }
+}
+print "<pre>$output</pre>";
 # C:\Users\spjones>tasklist  /fi "PID eq 17764"
 my $tasks = `tasklist | find /I "perl"`;
 $tasks =~ s/^perl.exe\s*/taskkill \/f \/pid /img;
 $tasks =~ s/(.*?\d+).*/$1/img;
 
-print $tasks;
+my $tasks_web = $tasks;
+$tasks_web =~ s/\n/<br>\n/img;
+print "<br>The task list:<pre>$tasks_web</pre><br>";
+
 while ($tasks =~ s/^(.*)\n//)
 {
     my $task = $1;
-    print ("Running.. $task..\n");
-    `$task`;
+    #print ("Running.. $task..\n");
+    #`$task`;
 }
-
-#my $pid = fork; exec "d:\\perl_programs\\abadice\\abadice.pl" if not $pid;
-
-#d:\\perl_programs\\cthulhu\\cthulhu.pl
-#d:\\perl_programs\\describe_magic_cards.pl
-#d:\\perl_programs\\draft_magic_auto_normal.pl
-#d:\\perl_programs\\draft_magic_cube.pl
-#d:\\perl_programs\\filter_magic.pl
-#d:\\perl_programs\\get_magic_deck.pl
-#d:\\perl_programs\\nothanks\\nothanks.pl
-#d:\\perl_programs\\quest\\quest.pl
